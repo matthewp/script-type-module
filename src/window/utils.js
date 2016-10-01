a8869808931b5524574e7b14daab0238ff42414b
Matthew Phillips <matthew@matthewphillips.info>
2016-10-01 12:48:41 -0400

    Remove the check for native support
    Instead people should just use the native support when it is available
    in the browsers they use for development.

--- src/window/utils.js
+++ src/window/utils.js
@@ -1,16 +1,3 @@
-export function hasNativeSupport(){
-  var script = document.createElement('script');
-  script.type = 'module';
-  var tempVar = '_scriptModuleSupported';
-  script.textContnet = 'window._scriptModuleSupported = true;';
-  document.head.appendChild(script);
-  document.head.appendChild(script);
-  var val = !!window[tempVar];
-  delete window[tempVar];
-  script.parentNode.removeChild(script);
-  return val;
-}
-
 export function currentScript() {
   return document.currentScript || document._currentScript || getCurrentScriptTheHardWay();
 }

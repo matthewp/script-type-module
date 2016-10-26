5dfda19d4edf0eea9bd492d7a07eb9de97a1c98b
Matthew Phillips <matthew@matthewphillips.info>
2016-10-26 17:11:13 -0400

    Add absolute URL test for import()

--- test/tests/import-fn/dynamic.js
+++ test/tests/import-fn/dynamic.js
@@ -4,9 +4,12 @@ let barPromise = import('./src/bar.js');
 let quxPromise = import('./src/qux.js').then(function(qux){
   return qux;
 });
+let pageUrl = new URL('./tests/import-fn/src/baz.js', document.baseURI);
+let bazPromise = import(pageUrl);
 
 self.RESULT = {
   foo: foo,
   bar: barPromise,
+  baz: bazPromise,
   qux: quxPromise
 };
